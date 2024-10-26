637e4167fe664bef9ca823e6a93e4c1024e40a1d
Saief <A.S.Bokor@outlook.com>
2024-10-26 20:02:44 +0100

    Remove First and Last Character

---
+++ JavaScript/Remove First and Last Character/script.js
@@ -0,0 +1,20 @@
+function removeChar(str){
+
+    let splitString = str.split('');
+        console.log("splitString: " + splitString);
+    let removeFirstChar = splitString.shift()
+        console.log("removeFirstChar: " + splitString)
+    let removeLastChar = splitString.pop()
+        console.log("removeLastChar" + splitString)
+    
+    let joinedString = splitString.join('')
+        console.log(joinedString)
+
+    return joinedString
+    
+   };
+   
+removeChar('hootki')
+   
+   
+   
