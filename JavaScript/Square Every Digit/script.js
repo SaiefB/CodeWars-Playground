a829813828e00bd23bdf4ec7c6672c86689ed5aa
Saief <A.S.Bokor@outlook.com>
2024-10-27 21:33:27 +0000

    Square Every Digit Completed

---
+++ JavaScript/Square Every Digit/script.js
@@ -0,0 +1,20 @@
+function squareDigits(num){
+    let toString = num.toString()
+    let split = toString.split('')
+    let squaredArr = []
+    let joinedArr = ''
+    console.log("Split Array: " + split)
+
+    for (let i = 0; i < split.length; i++){
+        let squared = split[i] * split[i]
+        squaredArr.push(squared)
+        console.log(squaredArr)
+        joinedArr = squaredArr.join('')
+    }
+    console.log(joinedArr)
+    return Number(joinedArr)
+  }
+
+
+
+squareDigits(9119)
