5acf8bba650fb4b3e80165f1b3a8748156ad83e4
Saief <A.S.Bokor@outlook.com>
2024-10-28 20:11:00 +0000

    Descending Order Completed

--- JavaScript/Descending Order/script.js
+++ JavaScript/Descending Order/script.js
@@ -1,3 +1,9 @@
 function descendingOrder(n){
-    //...
+    let split = n.toString().split('').sort(function(a, b) {
+      return b - a
+    }).join('')
+    console.log(split)
+    return Number(split)
   }
+
+descendingOrder(123456789)
