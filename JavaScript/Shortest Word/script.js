936d06d881d52f6975f781bfa079258b1b813ee3
Saief <A.S.Bokor@outlook.com>
2024-11-26 14:47:05 +0000

    Shortest Word Completed

--- JavaScript/Shortest Word/script.js
+++ JavaScript/Shortest Word/script.js
@@ -1,2 +1,12 @@
 function findShort(s){
+    const split = s.split(' ')
+    console.log(split)
+    const wordLengths = split.map(word => word.length);
+    console.log(wordLengths)
+    const sortedArr = wordLengths.sort((a, b) => a - b);
+    console.log(sortedArr)
+    console.log(sortedArr[0])
+    return sortedArr[0]
 }
+
+findShort("bitcoin take over the world maybe who knows perhaps")
