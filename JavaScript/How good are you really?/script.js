7ef9d3fef3af3384a9e97dcb727aaad3721656e4
Saief <A.S.Bokor@outlook.com>
2026-02-13 13:59:06 +0000

    complete kata

--- JavaScript/How good are you really?/script.js
+++ JavaScript/How good are you really?/script.js
@@ -1,3 +1,16 @@
 function betterThanAverage(classPoints, yourPoints) {
-  const classAverage = classPoints;
+  const initialValue = 0;
+  console.log("Initial Value: " + initialValue);
+  const sumWithInitial = classPoints.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue,
+  );
+  console.log("Sum with initial value: " + sumWithInitial);
+  const average = sumWithInitial / classPoints.length;
+  console.log("Average: " + average);
+  if (yourPoints > average) {
+    return true;
+  } else {
+    return false;
+  }
 }
