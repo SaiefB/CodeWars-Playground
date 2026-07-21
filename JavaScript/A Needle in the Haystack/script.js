b085f24fa2c972f66ba9a6c283e2441fda32f76d
Saief <A.S.Bokor@outlook.com>
2026-07-21 22:05:18 +0100

    start writing out the logic for the code to complete this exercise

--- JavaScript/A Needle in the Haystack/script.js
+++ JavaScript/A Needle in the Haystack/script.js
@@ -1,3 +1,6 @@
 function findNeedle(haystack) {
-  // your code here
+  // Loop through array
+  // for each array item check if the item is equal to "needle"
+  // if found return "found the needle at position _"
+  // if not found return "your function didn't return anything"
 }
