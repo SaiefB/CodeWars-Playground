464521c9b138488a69d0801f3b6944bb1d1096b7
Saief <A.S.Bokor@outlook.com>
2026-07-22 04:12:35 +0100

    wip

--- JavaScript/Invert Values/script.js
+++ JavaScript/Invert Values/script.js
@@ -1,3 +1,20 @@
 function invert(array) {
-  return;
+  // iterate through array
+  // if item is < 0 then value * 2 and + to item
+  // then add item to new array
+  // return array
+  // if item is > 0 then value * 2 and - to item
+  // then add item to new array
+  // return array
+  let newArray = [];
+  let currentNum = 0;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] < 0) {
+      console.log("array item less than 0");
+      currentNum += array[i] * 2;
+      console.log("current num: " + currentNum);
+      newArray.push(array[i]);
+      console.log(newArray);
+    }
+  }
 }
