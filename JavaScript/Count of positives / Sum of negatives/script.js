a14fe2f115df092255deac04a6d78dc232bd0dd2
Saief <A.S.Bokor@outlook.com>
2026-07-23 19:53:52 +0100

    complete exercise

--- JavaScript/Count of positives / Sum of negatives/script.js
+++ JavaScript/Count of positives / Sum of negatives/script.js
@@ -1,3 +1,35 @@
 function countPositivesSumNegatives(input) {
-  // your code here
+  if (input === null || input.length === 0) {
+    return [];
+  } else {
+    let positiveNum = [];
+    let negativeNum = [];
+    for (i = 0; i < input.length; i++) {
+      if (input[i] > 0) {
+        positiveNum.push(input[i]);
+        console.log(`positiveNum array: ${positiveNum}`);
+      } else {
+        negativeNum.push(input[i]);
+        console.log(`negativeNum array: ${negativeNum}`);
+      }
+    }
+
+    let positiveResult = positiveNum.length;
+    console.log("positiveResult: " + positiveResult);
+
+    let negSum = 0;
+    let negativeResult = negativeNum.forEach((num) => {
+      negSum += num;
+    });
+    console.log("negativeResult: " + negativeResult);
+    console.log("negSum: " + negSum);
+
+    let finalArray = [];
+    finalArray.push(positiveResult);
+    console.log(`finalArray: ${finalArray}`);
+    finalArray.push(negSum);
+    console.log(`finalArray: ${finalArray}`);
+
+    return finalArray;
+  }
 }
