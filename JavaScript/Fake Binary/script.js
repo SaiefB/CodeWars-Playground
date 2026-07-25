10258d8212867f2a7dde9dcf2b75cef1aaff49e7
Saief <A.S.Bokor@outlook.com>
2026-07-25 20:02:07 +0100

    complete fake binary exercise

--- JavaScript/Fake Binary/script.js
+++ JavaScript/Fake Binary/script.js
@@ -1,1 +1,15 @@
-function fakeBin(x) {}
+function fakeBin(x) {
+  let newArray = [];
+
+  for (i = 0; i < x.length; i++) {
+    if (x[i] >= 5) {
+      newArray.push(1);
+    } else {
+      newArray.push(0);
+    }
+    console.log(newArray);
+  }
+  let result = newArray.join("");
+  console.log(result);
+  return result;
+}
