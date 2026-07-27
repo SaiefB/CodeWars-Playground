fe0f82ed0b3a761f95ea5be956a08dd984009eb2
Saief <A.S.Bokor@outlook.com>
2026-07-27 21:10:42 +0100

    complete RNA to DNA Conversion exercise

--- JavaScript/DNA to RNA Conversion/script.js
+++ JavaScript/DNA to RNA Conversion/script.js
@@ -1,3 +1,19 @@
 function DNAtoRNA(dna) {
-  // create a function which returns an RNA sequence from the given DNA sequence
+  let rnaStrand = [];
+  let dnaStrandString = dna.split("");
+  console.log(dnaStrandString);
+
+  for (i = 0; i < dna.length; i++) {
+    if (dna[i] === "T") {
+      rnaStrand.push("U");
+      console.log(rnaStrand);
+    } else {
+      rnaStrand.push(dna[i]);
+      console.log(rnaStrand);
+    }
+  }
+
+  let finalRNAStand = rnaStrand.join("");
+  console.log(finalRNAStand);
+  return finalRNAStand;
 }
