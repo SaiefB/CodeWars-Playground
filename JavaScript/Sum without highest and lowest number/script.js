84c43d8d13ce7d37c46c6a64ea6d24ecbba974b4
Saief <A.S.Bokor@outlook.com>
2026-08-01 18:16:12 +0100

    complete Sum without highest and lowest number

--- JavaScript/Sum without highest and lowest number/script.js
+++ JavaScript/Sum without highest and lowest number/script.js
@@ -1,1 +1,17 @@
-function sumArray(array) {}
+function sumArray(array) {
+  if (array == null || array.length < 3) {
+    console.log("Invalid array returning 0");
+    return 0;
+  } else {
+    console.log("Valid array continuing...");
+    array.sort(function (a, b) {
+      return a - b;
+    });
+    console.log("Sorted array: " + array);
+
+    array.splice([0], 1);
+    array.pop();
+    console.log("spliced and popped array: " + array);
+    return array.reduce((a, b) => a + b, 0);
+  }
+}
