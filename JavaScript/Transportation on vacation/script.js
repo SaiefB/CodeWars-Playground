7f55354eeffa55f9646f0f10da3a188d803b30cc
Saief <A.S.Bokor@outlook.com>
2026-08-02 11:07:14 +0100

    complete Transportation for vacation exercise

--- JavaScript/Transportation on vacation/script.js
+++ JavaScript/Transportation on vacation/script.js
@@ -1,3 +1,9 @@
 function rentalCarCost(d) {
-  // Your solution here
+  if (d <= 2) {
+    return d * 40;
+  } else if (2 < d && d < 7) {
+    return d * 40 - 20;
+  } else {
+    return d * 40 - 50;
+  }
 }
