326fe22e95ab9a507f4d96e612142c87bb2eab70
Saief <A.S.Bokor@outlook.com>
2026-08-30 21:17:45 +0100

    completes the Get the mean of an array exercise

--- JavaScript/Get the mean of an array/script.js
+++ JavaScript/Get the mean of an array/script.js
@@ -1,3 +1,15 @@
 function getAverage(marks) {
-  //TODO : calculate the downward rounded average of the marks array
+  // get the sum of all the numbers in the array
+  const sum = marks.reduce(
+    (accumulater, currentValue) => accumulater + currentValue,
+    0,
+  );
+  console.log("sum: " + sum);
+  // divide by the number of array items length
+  const mean = sum / marks.length;
+  console.log("mean: " + mean);
+  //round down the number
+  const roundedMean = Math.floor(mean);
+  console.log("RoundedMean: " + roundedMean);
+  return roundedMean;
 }
